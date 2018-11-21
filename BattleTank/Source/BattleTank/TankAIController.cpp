906c4d9c0bfc4da9fc9113992c7944147a0ab87e
Coulibaly Falla Zoumana <bestcoulibaly10@hotmail.fr>
2018-11-21 11:41:31 -0500

    BT17: Get the Player Controller with C++

--- BattleTank/Source/BattleTank/TankAIController.cpp
+++ BattleTank/Source/BattleTank/TankAIController.cpp
@@ -5,18 +5,24 @@
 void ATankAIController::BeginPlay()
 {
 	Super::BeginPlay();
-	ATank* ControlledTank = GetControlledTank();
-	if (ControlledTank)
+	ATank* PlayerTank = GetPlayerTank();
+	if (PlayerTank)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("TankAIController possessing: %s"), *(ControlledTank->GetName()));
+		UE_LOG(LogTemp, Warning, TEXT("TankAIController found player: %s"), *(PlayerTank->GetName()));
 	}
 	else
 	{
-		UE_LOG(LogTemp, Warning, TEXT("TankAIController not possessing a tank"));
+		UE_LOG(LogTemp, Warning, TEXT("TankAIController can't find player tank"));
 	}
 }
 
 ATank* ATankAIController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	APawn* PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	return Cast<ATank>(PlayerPawn);
 }
--- BattleTank/Source/BattleTank/TankAIController.h
+++ BattleTank/Source/BattleTank/TankAIController.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "Tank.h"
 #include "AIController.h"
+#include "TankPlayerController.h"
 #include "TankAIController.generated.h"
 
 /**
@@ -18,4 +19,6 @@ class BATTLETANK_API ATankAIController : public AAIController
 	virtual void BeginPlay() override;
 
 	ATank* GetControlledTank() const;
+
+	ATank* GetPlayerTank() const;
 };
