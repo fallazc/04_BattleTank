9a7cbda991c86acc3fba553610741d404dc09120
Coulibaly Falla Zoumana <bestcoulibaly10@hotmail.fr>
2018-11-21 11:19:40 -0500

    BT16: Creating an AI Controller Class

---
+++ BattleTank/Source/BattleTank/TankAIController.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	ATank* ControlledTank = GetControlledTank();
+	if (ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TankAIController possessing: %s"), *(ControlledTank->GetName()));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TankAIController not possessing a tank"));
+	}
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
